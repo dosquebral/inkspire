79ce66f1f3f90efaba1c7571e4826c59a7500cb7
DJ <imdarlenequebral@gmail.com>
2025-02-12 21:01:34 +0800

    add modal

--- frontend/src/components/AddEditNotes.jsx
+++ frontend/src/components/AddEditNotes.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
-const AddEdirNotes = () => {
+const AddEditNotes = () => {
   return (
-    <div>AddEdirNotes</div>
+    <div>
+      <div className='flex flex-col gap-2'>
+        <label className='input-label'>TITLE</label>
+        <input 
+          type="text"
+          placeholder='Go To Gym At 5'
+          className='text-2xl text-slate-950 outline-none'
+        />
+      </div>
+
+      <div className='flex flex-col gap-2 mt-4'>
+        <label className='input-label'>CONTENT</label>
+        <textarea 
+          type="text"
+          className='text-sm text-slate-50 outline-none bg-slate-400 p-2 rounded'
+          placeholder='Content'
+          rows={10}
+        />
+      </div>
+
+      <div className='mt-3'>
+        <label className='input-label'>TAGS</label>
+      </div>
+
+      <button className='btn-primary font-medium mt-5 p-3' onClick={() => {}}>
+        ADD
+      </button>
+    </div>
   )
 }
 
-export default AddEdirNotes
+export default AddEditNotes
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,8 +3,17 @@ import Navbar from '../components/Navbar';
 import NoteCard from '../components/NoteCard';
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from '../components/AddEditNotes';
+import { useState } from 'react';
+import Modal from 'react-modal';
 
 const HomePage = () => {
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  })
+
   return (
     <>
       <Navbar />
@@ -25,11 +34,33 @@ const HomePage = () => {
       </div>
       
 
-      <button className='w-16 h-16 flex items-center justify-center bg-[var(--secondary)] rounded-2xl absolute right-10 bottom-10 hover:bg-green-900'>
+      <button 
+        className='w-16 h-16 flex items-center justify-center bg-[var(--secondary)] rounded-2xl absolute right-10 bottom-10 hover:bg-green-900'
+        onClick={() => {
+          setOpenAddEditModal({isShown: true, type: "add", data: null})
+        }}
+      >
         <MdAdd className='text-white text-[32px]' />
       </button>
 
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.2)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md p-5 overflow-scroll"
+      >
+
       <AddEditNotes />
+
+      </Modal>
     </>
   )
 }
